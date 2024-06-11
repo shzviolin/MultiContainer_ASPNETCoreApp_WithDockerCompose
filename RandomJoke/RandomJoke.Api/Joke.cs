fa274494c2205605d88ac0861c3863a992b8d0a9
Ali Akbar Ebrahimi <a.ebrahimi1367@gmail.com>
2024-06-11 22:44:34 +0330

    03_Add logic of the application

---
+++ RandomJoke/RandomJoke.Api/Joke.cs
@@ -0,0 +1,9 @@
+﻿namespace RandomJoke.Api
+{
+    public class Joke
+    {
+        public string Name { get; set; }
+        public string Text { get; set; }
+        public string Category { get; set; }
+    }
+}
--- RandomJoke/RandomJoke.Web/Pages/Index.cshtml.cs
+++ RandomJoke/RandomJoke.Web/Pages/Index.cshtml.cs
@@ -1,20 +1,38 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
 
 namespace RandomJoke.Web.Pages
 {
+    public class JokeModel
+    {
+        public string Name { get; set; }
+        public string Text { get; set; }
+        public string Category { get; set; }
+    }
+
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+
+        public JokeModel Joke { get; set; }
 
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
         }
 
-        public void OnGet()
+        public async Task OnGetAsync()
         {
+            using (var client = new System.Net.Http.HttpClient())
+            {
+                var request = new System.Net.Http.HttpRequestMessage();
+                request.RequestUri = new Uri("http://localhost:5035/api/Joke");
+                var response = await client.SendAsync(request);
+                var jokeJson = await response.Content.ReadAsStringAsync();
 
+                Joke = JsonConvert.DeserializeObject<JokeModel>(jokeJson);
+            }
         }
     }
+
 }
